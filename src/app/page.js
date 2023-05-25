620d5b1624c200af6eff821626d3ce3bca032428
Ben Huang <byhuang4100@gmail.com>
2023-05-25 19:04:39 -0400

    finished intro card

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className={styles.body}>
-      {/* <Header /> */}
+      <Header />
       <Card />
       <div>HELLO</div>
     </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,23 @@
+import Image from 'next/image';
+import drawk from '../../../public/drawk.png';
 import styles from './Header.module.css';
 
 export default function Header() {
   return (
     <div className={styles.wrapper}>
+      <Image
+        className={styles.me}
+        src={drawk}
+        alt="me"
+        width={100}
+        height={100}
+      />
       <ul className={styles.links}>
         <li>
-          <a href="@/pages/About/About">About</a>
+          <a href="#">Cool Stuff</a>
         </li>
         <li>
-          <a>Work</a>
-        </li>
-        <li>
-          <a>Cool Stuff</a>
+          <a href="#">About</a>
         </li>
       </ul>
     </div>
